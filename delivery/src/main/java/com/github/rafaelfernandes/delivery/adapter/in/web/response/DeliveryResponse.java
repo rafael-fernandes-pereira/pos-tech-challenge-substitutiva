fcbe2d6c0e362506205f63c72bb8c4bb36e7622e
Rafael Fernandes Pereira <rafaelfernandes@ciandt.com>
2025-02-09 18:23:51 -0300

    Delivery API - Get by apartment

---
+++ delivery/src/main/java/com/github/rafaelfernandes/delivery/adapter/in/web/response/DeliveryResponse.java
@@ -0,0 +1,19 @@
+package com.github.rafaelfernandes.delivery.adapter.in.web.response;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public record DeliveryResponse(
+        String id,
+        Integer apartment,
+        String deliveryStatus,
+        String employeeName,
+        String destinationName,
+        String packageDescription,
+        String receiverName,
+        String notificationStatus,
+        LocalDate enterDate,
+        LocalDate exitDate
+) {
+
+}
---
+++ delivery/src/main/java/com/github/rafaelfernandes/delivery/adapter/out/persistence/DeliveryMapper.java
@@ -0,0 +1,56 @@
+package com.github.rafaelfernandes.delivery.adapter.out.persistence;
+
+import com.github.rafaelfernandes.delivery.application.domain.model.Delivery;
+import com.github.rafaelfernandes.delivery.application.domain.model.Employee;
+import com.github.rafaelfernandes.delivery.application.domain.model.Resident;
+import org.springframework.stereotype.Component;
+
+public class DeliveryMapper {
+
+    public static DeliveryJpaEntity toEntity(Delivery delivery) {
+        return DeliveryJpaEntity.builder()
+                .residentId(delivery.getResident().residentId().id())
+                .employeeId(delivery.getEmployee().employeeId().id())
+                .deliveryStatus(delivery.getDeliveryStatus())
+                .notificationStatus(delivery.getNotificationStatus())
+                .enterDate(delivery.getEnterDate())
+                .receiverName(delivery.getReceiverName())
+                .destinationName(delivery.getDestinationName())
+                .packageDescription(delivery.getPackageDescription())
+                .build();
+    }
+
+    public static Delivery toDomain(DeliveryJpaEntity deliveryJpaEntity) {
+
+        var resident = new Resident(
+                new Resident.ResidentId(deliveryJpaEntity.getResidentId()),
+                null,
+                null,
+                null,
+                null
+        );
+
+        var employee = new Employee(
+                new Employee.EmployeeId(deliveryJpaEntity.getEmployeeId()),
+                null,
+                null,
+                null
+        );
+
+        return Delivery.of(
+                deliveryJpaEntity.getId().toString(),
+                resident,
+                employee,
+                deliveryJpaEntity.getDestinationName(),
+                deliveryJpaEntity.getPackageDescription(),
+                deliveryJpaEntity.getDeliveryStatus(),
+                deliveryJpaEntity.getNotificationStatus(),
+                deliveryJpaEntity.getEnterDate(),
+                deliveryJpaEntity.getExitDate(),
+                deliveryJpaEntity.getReceiverName()
+        );
+
+    }
+
+
+}
